6daca01f5c512e0d7b9095d1794ae7dfce4a2032
yashsingh9242 <yashsingh9242@gmail.com>
2026-09-12 20:13:03 +0530

    Time: 2 ms (81.31%), Space: 46.7 MB (45.95%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,50 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+        solve(0, board, ans, n);
+        return ans;
+    }
+    private void solve(int row, char[][] board, List<List<String>> ans, int n) {
+        if (row == n) {
+            List<String> list = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                list.add(new String(board[i]));
+            }
+
+            ans.add(list);
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            if (isSafe(board, row, col, n)) {
+                board[row][col] = 'Q';
+
+                solve(row + 1, board, ans, n);
+
+                board[row][col] = '.';
+            }
+        }
+    }
+    private boolean isSafe(char[][] board, int row, int col, int n) {
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
